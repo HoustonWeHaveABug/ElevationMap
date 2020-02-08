0ff6003208d3b6d76af1997bfc4b0421adf825df
HoustonWeHaveABug <HoustonWeHaveABug@users.noreply.github.com>
2020-02-08 10:08:20 +0100

    Update elevation_map.c
    Added management of dynamic number of octaves.
    Normalized constants according to their type.

--- elevation_map.c
+++ elevation_map.c
@@ -8,11 +8,11 @@
 
 int main(int argc, char *argv[]) {
 	int elevation_min, elevation_max, html_output;
-	unsigned long height, width, map_size, y;
+	unsigned long height, width, octaves_n, map_size, y;
 	double *map, map_min, map_max;
 	int64_t seed;
-	if (argc != 1 || scanf("%lu%lu%d%d%"SCNi64"%d", &height, &width, &elevation_min, &elevation_max, &seed, &html_output) != 6 || height < 2 || width < 2 || elevation_min > elevation_max) {
-		fprintf(stderr, "%s: parameters expected on standard input: height width elevation_min elevation_max seed html_output\nheight > 1\nwidth > 1\nelevation_min <= elevation_max\n", argv[0]);
+	if (argc != 1 || scanf("%lu%lu%d%d%"SCNi64"%lu%d", &height, &width, &elevation_min, &elevation_max, &seed, &octaves_n, &html_output) != 7 || height < 2UL || width < 2UL || elevation_min > elevation_max || octaves_n == 0UL) {
+		fprintf(stderr, "%s: parameters expected on standard input: height width elevation_min elevation_max seed octaves_n html_output\nheight > 1\nwidth > 1\nelevation_min <= elevation_max\noctaves_n > 0\n", argv[0]);
 		fflush(stderr);
 		return EXIT_FAILURE;
 	}
@@ -29,14 +29,20 @@ int main(int argc, char *argv[]) {
 		return EXIT_FAILURE;
 	}
 	simplex_noise_init(seed);
-	map_min = 1;
-	map_max = -1;
-	for (y = 0; y < height; y++) {
+	map_min = 1.0;
+	map_max = -1.0;
+	for (y = 0UL; y < height; y++) {
 		unsigned long x;
-		double ny = y*2.0/height-1;
-		for (x = 0; x < width; x++) {
-			double nx = x*2.0/width-1;
-			map[y*width+x] = simplex_noise(nx, ny)+0.5*simplex_noise(2*nx, 2*ny)+0.25*simplex_noise(4*nx, 4*ny);
+		double ny = y*2.0/height-1.0;
+		for (x = 0UL; x < width; x++) {
+			unsigned long c;
+			double nx = x*2.0/width-1.0, frequency = 1.0, weight = 1.0;
+			map[y*width+x] = simplex_noise(nx, ny);
+			for (c = 1UL; c < octaves_n; c++) {
+				frequency *= 2.0;
+				weight /= 2.0;
+				map[y*width+x] += simplex_noise(frequency*nx, frequency*ny)*weight;
+			}
 			if (map[y*width+x] < map_min) {
 				map_min = map[y*width+x];
 			}
@@ -45,9 +51,9 @@ int main(int argc, char *argv[]) {
 			}
 		}
 	}
-	for (y = 0; y < height; y++) {
+	for (y = 0UL; y < height; y++) {
 		unsigned long x;
-		for (x = 0; x < width; x++) {
+		for (x = 0UL; x < width; x++) {
 			map[y*width+x] = (map[y*width+x]-map_min)*(elevation_max-elevation_min)/(map_max-map_min)+elevation_min;
 		}
 	}
@@ -68,18 +74,18 @@ int main(int argc, char *argv[]) {
 		puts("<BODY>");
 		puts("<H1>Elevation Map</H1>");
 		puts("<TABLE>");
-		printf("<CAPTION>Height %lu<BR>\nWidth %lu<BR>\nElevation Min. %d<BR>\nElevation Max. %d<BR>\nSeed %"PRIi64"</CAPTION>\n", height, width, elevation_min, elevation_max, seed);
-		for (y = 0; y < height; y++) {
+		printf("<CAPTION>Height %lu<BR>\nWidth %lu<BR>\nElevation Min. %d<BR>\nElevation Max. %d<BR>\nSeed %"PRIi64"<BR>Octaves %lu</CAPTION>\n", height, width, elevation_min, elevation_max, seed, octaves_n);
+		for (y = 0UL; y < height; y++) {
 			unsigned long x;
 			puts("<TR>");
-			for (x = 0; x < width; x++) {
+			for (x = 0UL; x < width; x++) {
 				int red, green;
 				if (map[y*width+x] < elevation_mid) {
-					red = 255-(int)((elevation_mid-map[y*width+x])/(elevation_mid-elevation_min)*255);
+					red = 255-(int)((elevation_mid-map[y*width+x])/(elevation_mid-elevation_min)*255.0);
 					green = 255;
 				}
 				else {
-					green = 255-(int)((map[y*width+x]-elevation_mid)/(elevation_max-elevation_mid)*255);
+					green = 255-(int)((map[y*width+x]-elevation_mid)/(elevation_max-elevation_mid)*255.0);
 					red = 255;
 				}
 				printf("<TD STYLE=\"background-color: #%02x%02x00;\"></TD>\n", red, green);
@@ -92,10 +98,10 @@ int main(int argc, char *argv[]) {
 	}
 	else {
 		printf("%lu %lu\n", height, width);
-		for (y = 0; y < height; y++) {
+		for (y = 0UL; y < height; y++) {
 			unsigned long x;
 			printf("%d", (int)map[y*width]);
-			for (x = 1; x < width; x++) {
+			for (x = 1UL; x < width; x++) {
 				printf(" %d", (int)map[y*width+x]);
 			}
 			puts("");
